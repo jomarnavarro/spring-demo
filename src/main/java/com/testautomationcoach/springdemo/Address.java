73bdf91b89760da7bcb135fbb37e164c4c357fc3
Omar Navarro <jomarnavarro@gmail.com>
2021-05-12 14:36:19 -0500

    Adds default value for variables

--- src/main/java/com/testautomationcoach/springdemo/Address.java
+++ src/main/java/com/testautomationcoach/springdemo/Address.java
@@ -1,15 +1,12 @@
 package com.testautomationcoach.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Address {
-
+    @Value("${address.default:123 main street}")
     public String street;
-
-    public Address() {
-        this.street = "101 non main street";
-    }
 
     public String getStreet() {
         return this.street;
